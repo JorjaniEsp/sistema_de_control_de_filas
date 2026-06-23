052cc62100f46d8631547e92a4f39cce258520ef
JorjaniEsp <espinozajorjanie2006@gmail.com>
2026-06-23 01:45:00 -0600

    kiosco listo

---
+++ controlador/KioscoController.js
@@ -0,0 +1,96 @@
+let idAreaSeleccionada = null;
+let prefijoAreaSeleccionada = null;
+
+$('#CrearTiquete').prop('disabled', true);
+
+$('.servicio').click(function () {
+    $('.servicio').removeClass('seleccionado');
+    $(this).addClass('seleccionado');
+
+    idAreaSeleccionada = $(this).data('id');
+    prefijoAreaSeleccionada = $(this).data('prfijo');
+
+    verificarCampos();
+});
+
+$('#nombre').on('input', function () {
+    verificarCampos();
+});
+
+function verificarCampos() {
+    let nombre = $('#nombre').val().trim();
+    
+    if (nombre.length >= 3 && idAreaSeleccionada !== null) {
+        $('#CrearTiquete').prop('disabled', false);
+    } else {
+        $('#CrearTiquete').prop('disabled', true);
+    }
+}
+
+$('#CrearTiquete').click(function () {
+    $(this).prop('disabled', true).text('Procesando...');
+
+    let nombrePaciente = $('#nombre').val().trim();
+
+    $.ajax({
+        url: '../server/kiosco.php',
+        type: 'post',                      
+        dataType: 'json',                  
+        data: { 
+            accion : 1,
+            nombre: $('#nombre').val().trim(),
+            area: prefijoAreaSeleccionada,
+            idArea: idAreaSeleccionada
+        }
+    }).done(function (respuesta){
+        mostrarTiquete(respuesta[0]);
+    })
+});
+
+function mostrarTiquete(idTiquete){
+    $.ajax({
+        url: '../server/kiosco.php',
+        type: 'post',                      
+        dataType: 'json',                  
+        data: { 
+            accion : 2,
+            idTiquete: idTiquete,
+        }
+    }).done(function (respuesta){
+
+        let fila = respuesta[0];
+        let departamento = '';
+
+        if(fila.id_area == 1) {
+            departamento = 'Ventanilla'
+        } else if(fila.id_area == 2){
+            departamento = 'Consultorio'
+        } else if(fila.id_area == 3){
+            departamento = 'Enfermeria'
+        } else {
+            departamento = 'Farmacia'
+        }
+
+        let html = `
+        <div id="tiquete">
+            <div id="target">
+                <h2>${fila.codigo}</h2>
+                <p>${departamento}</p>
+            </div>
+            
+            <div id="info-tiquete">
+                <div class="info-item">
+                    <span class="label">Posición en cola</span>
+                    <span class="value">1</span>
+                </div>
+                
+                <div class="info-item no-border">
+                    <span class="value small-text">Espere ser llamado en las pantallas</span>
+                </div>
+            </div>
+        </div>
+    `;
+
+    $('#contenedor-btnServicios').html(html);
+    })
+}
--- server/kiosco.php
+++ server/kiosco.php
@@ -1,18 +1,24 @@
 <?php
 
-include './server/conexion.php';
+$conexion = mysqli_connect("localhost","root","","sistema_gestion_filas")
+    or die(mysqli_connect_error());
 
-$nombre = $_POST['nombre'];
-$area = $_POST['area'];
-$idArea = $_POST['idArea'];
 $accion = $_POST['accion'];
 
 if($accion == '1'){
+    $nombre = $_POST['nombre'];
+    $area = $_POST['area'];
+    $idArea = $_POST['idArea'];
     $idPaciente = insertarPaciente($conexion,$nombre);
     $infoTiquete = crearTiquete($conexion,$idArea,$area,$idPaciente);
     insertarHistorial($conexion, $infoTiquete[0],$idArea);
     echo json_encode($infoTiquete);
+} else if($accion == '2') {
+    $idTiquete = $_POST['idTiquete'];
+    echo json_encode(obtenerTiquete($conexion,$idTiquete));
+
 } else {
+    $idArea = $_POST['idArea'];
     $espera = filaEspera($conexion,$idArea);
     echo json_encode($espera);
 }
@@ -25,7 +31,7 @@ function insertarPaciente($conexion, $nombre){
 }
 
 function crearTiquete($conexion,$idArea,$area,$idPaciente){
-    $num = obtenerUltimoId($conexion,$area) + 1;
+    $num = obtenerUltimoId($conexion,$idArea) + 1;
     $codigo = "";
     if ($num < 10) {
         $codigo = $area . "-00" . $num;
@@ -65,6 +71,21 @@ function filaEspera($conexion, $idArea){
         array_push($filas, $array);
     }
     return $filas;
+}
+
+function obtenerTiquete($conexion, $idTiquete){
+    
+        $sql = "SELECT historial_atencion.estado, historial_atencion.id_area, historial_atencion.fecha_creacion, tiquete.codigo FROM historial_atencion
+        JOIN tiquete ON historial_atencion.id_tiquete = tiquete.id_tiquete
+        WHERE historial_atencion.id_tiquete =" . $idTiquete;
+
+        $datos = mysqli_query($conexion, $sql);
+
+        $filas = array();
+        while ($array = mysqli_fetch_array($datos)) {
+            array_push($filas, $array);
+        }
+        return $filas;
 }
 
 ?>
