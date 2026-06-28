a1a3b19f8d8fd6c65197e43a53c4c3649dde2aac
JorjaniEsp <espinozajorjanie2006@gmail.com>
2026-06-28 15:50:44 -0600

    feat: logica del login lista

---
+++ controlador/loginController.js
@@ -0,0 +1,29 @@
+$('#ingresar').click( function(){
+    let InputUsuario = $('#input-usuario').val();
+    let InputPassword = $('#input-contraseña').val();
+
+    console.log("Pass " + InputPassword);
+    console.log("Usu " +  InputUsuario);
+    
+    
+    if(InputUsuario.length == 0 || InputPassword.length == 0){
+        $('#mensaje').text('Debe ingresar su usuario y contraseña');
+        return;
+    }
+
+    $.ajax({
+        url: './server/login.php',
+        type: 'post',
+        dataType: 'json',
+        data : {
+            usuario : InputUsuario,
+            password : InputPassword
+        }
+    }).done(function(respuesta){
+        if(respuesta.exito === false){
+            $('#mensaje').text('Usuario o Contraseña incorrectos');
+        } else {
+            window.location.href = ''
+        }
+    })
+})
---
+++ server/login.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+$conexion = mysqli_connect("localhost","root","","sistema_gestion_filas")
+    or die(mysqli_connect_error());
+
+$usuario = $_POST['usuario'];
+$password = $_POST['password'];
+
+$resultado = verificarLogin($conexion, $usuario, $password);
+echo json_encode($resultado);
+
+
+function verificarLogin($conexion, $usuario, $password){
+
+    $sql = "SELECT t.id_trabajador, t.nombre, t.password, t.id_area, t.id_rol, a.nombre as nombreArea, r.nombre as nombreRol
+    FROM trabajador as t
+    JOIN area as a ON t.id_area = a.id_area
+    JOIN rol as r ON t.id_rol = r.id_rol
+    WHERE t.cedula = '" . $usuario . "'";
+
+    $datos = mysqli_query($conexion, $sql);
+    $fila = mysqli_fetch_array($datos);
+
+    if($fila == null){
+        return ["exito" => false];
+    }
+
+    if($fila['password'] != $password){
+        return ["exito" => false];
+    }
+
+    $_SESSION['id_trabajador'] = $fila['id_trabajador'];
+    $_SESSION['id_area'] = $fila['id_area'];
+    $_SESSION['id_rol'] = $fila['id_rol'];
+
+    return [
+        "exito" => true,
+        "nombre" => $fila['nombre'],
+        "idArea" => $fila['id_area'],
+        "nombreArea" => $fila['nombreArea'],
+        "nombreRol" => $fila['nombreRol']
+    ];
+}
+
+?>
